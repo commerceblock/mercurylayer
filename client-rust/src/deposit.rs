f03e35234157f78b73d0b65880a6c70f65e9aec9
S. Santos <ssantosj21@gmail.com>
2023-11-02 19:34:21 -0300

    Add create_signature in rust lib

--- client-rust/src/deposit.rs
+++ client-rust/src/deposit.rs
@@ -85,7 +85,7 @@ pub async fn execute(client_config: &ClientConfig, wallet_name: &str, token_id: 
     let client_partial_sig_hex = partial_sig_request.client_partial_sig;
     let server_partial_sig_hex = hex::encode(server_partial_sig.serialize());
     let msg = partial_sig_request.msg;
-    let session_hex = server_partial_sig_request.session;
+    let session_hex = partial_sig_request.encoded_session;
     let output_pubkey_hex = partial_sig_request.output_pubkey;
     
     let _ = create_signature(msg, client_partial_sig_hex, server_partial_sig_hex, session_hex, output_pubkey_hex)?;
