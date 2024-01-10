e074afcbd300ed4320045fbe0ae6acf25aa0fa84
Thomas Trevethan <ttrevethan@Thomass-MacBook-Pro.local>
2024-01-10 16:54:34 +0000

    config

--- token-server/migrations/0001_key_data_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE public.tokens (
-	id serial4 NOT NULL,
-	token_id varchar NULL UNIQUE,
-	invoice varchar NULL,
-	onchain_address varchar NULL,
-	processor_id varchar NULL,
-	confirmed boolean DEFAULT false,
-	spent boolean DEFAULT false
-);
