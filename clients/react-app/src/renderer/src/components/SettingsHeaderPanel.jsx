05a86541f1ae306953e5d29ab0c9029f41e37555
rk16449 <rajkadirx@gmail.com>
2024-02-19 15:10:05 +0000

    chore: remove manage transactions from settings page as will be in modal

--- clients/react-app/src/renderer/src/components/SettingsHeaderPanel.jsx
+++ clients/react-app/src/renderer/src/components/SettingsHeaderPanel.jsx
@@ -29,10 +29,10 @@ const SettingsHeaderPanel = ({ wallet }) => {
     activity_data = JSON.stringify(activity_data)
 
     var a = document.createElement('a')
-    var file = new Blob([activity_data], { type: 'text/plain' }) //text/plain
+    var file = new Blob([activity_data], { type: 'application/json' })
 
     a.href = URL.createObjectURL(file)
-    a.download = 'activity.txt'
+    a.download = 'activity.json'
     a.click()
   }
 
@@ -56,18 +56,19 @@ const SettingsHeaderPanel = ({ wallet }) => {
           </div>
         </div>
       </div>
-      <div className="self-stretch bg-white h-[39px] flex flex-row items-center justify-between py-[19px] px-[18px] box-border">
-        <button className="cursor-pointer [border:none] p-0 bg-mediumslateblue-200 w-[162px] rounded-sm shadow-[0px_2px_2px_rgba(0,_0,_0,_0.25)] h-[27px] overflow-hidden shrink-0 flex flex-row items-center justify-center">
+      <div className="self-stretch bg-white h-[39px] flex flex-row items-center justify-end py-[19px] px-[18px] box-border">
+        {/*
+          <button className="cursor-pointer [border:none] p-0 bg-mediumslateblue-200 w-[162px] rounded-sm shadow-[0px_2px_2px_rgba(0,_0,_0,_0.25)] h-[27px] overflow-hidden shrink-0 flex flex-row items-center justify-center">
           <div className="self-stretch flex-1 relative text-xs tracking-[-0.02em] leading-[22px] font-semibold font-body-heavy text-white text-center flex items-center justify-center">
             MANAGE TRANSACTIONS
           </div>
-        </button>
+        </button> */}
         <button
           onClick={() => downloadWalletBackup()}
           className="cursor-pointer [border:none] p-0 bg-mediumslateblue-200 w-[165px] rounded-sm shadow-[0px_2px_2px_rgba(0,_0,_0,_0.25)] h-[27px] overflow-hidden shrink-0 flex flex-row items-center justify-center"
         >
           <div className="self-stretch flex-1 relative text-xs tracking-[-0.02em] leading-[22px] font-semibold font-body-heavy text-white text-center flex items-center justify-center">
-            CREATE WALLET BACKUP
+            EXPORT WALLET BACKUP
           </div>
         </button>
       </div>
