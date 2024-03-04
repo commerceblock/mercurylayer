303cdab7a8c19c61c44adcece9d5a07722aa0227
rk16449 <rajkadirx@gmail.com>
2024-03-04 17:08:47 +0000

    chore: change app icon

--- clients/react-app/src/main/index.js
+++ clients/react-app/src/main/index.js
@@ -1,4 +1,7 @@
 const { app, BrowserWindow, dialog, ipcMain, shell, nativeTheme, session } = require('electron')
+import { Tray, nativeImage } from 'electron'
+import appIcon from '../../resources/icon.png?asset'
+
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
@@ -61,6 +64,8 @@ app.whenReady().then(async () => {
   installExtension(REDUX_DEVTOOLS)
     .then((name) => console.log(`Added Extension:  ${name}`))
     .catch((err) => console.log('An error occurred: ', err))
+
+  let tray = new Tray(nativeImage.createFromPath(appIcon))
 
   let db
 
