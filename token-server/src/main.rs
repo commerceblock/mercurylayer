3cfdab5e409abe5baa2ad6bce16e54c389703704
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2024-03-27 21:10:03 +0530

    fix: config for token server port

--- token-server/src/main.rs
+++ token-server/src/main.rs
@@ -44,7 +44,7 @@ async fn main() {
         .manage(token_server)
         .attach(Cors)
         // .attach(MercuryPgDatabase::fairing())
-        .listen(8001)
+        .configure(rocket::Config::figment().merge(("port", 8001)))
         .launch()
         .await;
 }
