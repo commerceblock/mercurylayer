818831a792fdd7b6ade8e5231b1722253c2f6f5a
S. Santos <ssantosj21@gmail.com>
2024-04-17 02:11:58 -0300

    Add docker files to the server

--- server/src/server_config.rs
+++ server/src/server_config.rs
@@ -1,5 +1,6 @@
 use config::Config;
 use serde::{Serialize, Deserialize};
+use std::env;
 
 /// Config struct storing all StataChain Entity config
 #[derive(Debug, Serialize, Deserialize)]
@@ -23,12 +24,17 @@ impl ServerConfig {
             .build()
             .unwrap();
 
+        // Function to fetch a setting from the environment or fallback to the config file
+        let get_env_or_config = |key: &str, env_var: &str| -> String {
+            env::var(env_var).unwrap_or_else(|_| settings.get_string(key).unwrap())
+        };
+
         ServerConfig {
-            lockbox: Some(settings.get_string("lockbox").unwrap()),
-            network: settings.get_string("network").unwrap(),
-            lockheight_init: settings.get_int("lockheight_init").unwrap() as u32,
-            lh_decrement: settings.get_int("lh_decrement").unwrap() as u32,
-            connection_string: settings.get_string("connection_string").unwrap(),
+            lockbox: Some(get_env_or_config("lockbox", "LOCKBOX_URL")),
+            network: get_env_or_config("network", "BITCOIN_NETWORK"),
+            lockheight_init: get_env_or_config("lockheight_init", "LOCKHEIGHT_INIT").parse::<u32>().unwrap(),
+            lh_decrement: get_env_or_config("lh_decrement", "LH_DECREMENT").parse::<u32>().unwrap(),
+            connection_string: get_env_or_config("connection_string", "DATABASE_CONNECTION_STRING"),
         }
     }
 }
