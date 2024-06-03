4a8e0a5a07d76f4fa4087ec74f7ad1e0efdc6bda
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2024-06-03 14:39:05 +0530

    debug: depositMsg for /deposit/init/pod

--- clients/libs/nodejs/deposit.js
+++ clients/libs/nodejs/deposit.js
@@ -93,7 +93,7 @@ const init = async (clientConfig, db, wallet, token_id) => {
     if (torProxy) {
         socksAgent = { httpAgent: new SocksProxyAgent(torProxy) };
     }
-
+    console.log('depositMsg1 ->', depositMsg1)
     const response = await axios.post(url, depositMsg1, socksAgent);
 
     if (response.status != 200) {
