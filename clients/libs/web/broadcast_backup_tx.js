648890a461c72fe7f25cdbcc273af1477d99ac80
S. Santos <ssantosj21@gmail.com>
2024-06-06 13:52:59 -0300

    Add web client lib

---
+++ clients/libs/web/broadcast_backup_tx.js
@@ -0,0 +1,83 @@
+import axios from 'axios';
+import initWasm from 'mercury-wasm';
+import wasmUrl from 'mercury-wasm/mercury_wasm_bg.wasm?url'
+import * as mercury_wasm from 'mercury-wasm';
+import storageManager from './storage_manager.js';
+import utils from './utils.js';
+import CoinStatus from './coin_enum.js';
+import transaction from './transaction.js';
+
+const execute = async (clientConfig, walletName, statechainId, toAddress, feeRate) => {
+
+    await initWasm(wasmUrl);
+
+    let wallet = storageManager.getItem(walletName);
+
+    let backupTxs = storageManager.getItem(statechainId);
+
+    if (!feeRate) {
+        const serverInfo = await utils.infoConfig(clientConfig);
+        const feeRateSatsPerByte = serverInfo.feeRateSatsPerByte;
+        feeRate = feeRateSatsPerByte;
+    } else {
+        feeRate = parseInt(feeRate, 10);
+    }
+
+    let coinsWithStatechainId = wallet.coins.filter(c => {
+        return c.statechain_id === statechainId
+    });
+
+    if (!coinsWithStatechainId) {
+        throw new Error(`There is no coin for the statechain id ${statechainId}`);
+    }
+
+    // If the user sends to himself, he will have two coins with same statechain_id
+    // In this case, we need to find the one with the lowest locktime
+    // Sort the coins by locktime in ascending order and pick the first one
+    let coin = coinsWithStatechainId.sort((a, b) => a.locktime - b.locktime)[0];
+
+    if (coin.status != CoinStatus.CONFIRMED && coin.status != CoinStatus.IN_TRANSFER) {
+        throw new Error(`Coin status must be CONFIRMED or IN_TRANSFER to transfer it. The current status is ${coin.status}`);
+    }
+
+    const backupTx = mercury_wasm.latestBackuptxPaysToUserpubkey(backupTxs, coin, wallet.network);
+
+    if (!backupTx) {
+        throw new Error(`There is no backup transaction for the statechain id ${statechainId}`);
+    }
+
+    const CpfpTx = mercury_wasm.createCpfpTx(backupTx, coin, toAddress, feeRate, wallet.network);
+
+    const url = `${clientConfig.esploraServer}/api/tx`;
+
+    let response = await axios.post(url, backupTx.tx, {
+        headers: {
+            'Content-Type': 'text/plain'
+        }
+    });
+
+    let backupTxTxid = response.data;
+
+    response = await axios.post(url, CpfpTx, {
+        headers: {
+            'Content-Type': 'text/plain'
+        }
+    });
+
+    let cpfpTxTxid = response.data;
+
+    coin.tx_cpfp = cpfpTxTxid;
+    coin.withdrawal_address = toAddress;
+    coin.status = CoinStatus.WITHDRAWING;
+
+    storageManager.setItem(walletName, wallet, true);
+
+    utils.completeWithdraw(clientConfig, coin.statechain_id, coin.signed_statechain_id);
+
+    return {
+        backupTx: backupTxTxid,
+        cpfpTx: cpfpTxTxid
+    };
+}
+
+export default { execute };
---
+++ clients/libs/web/utils.js
@@ -0,0 +1,71 @@
+import axios from 'axios';
+
+const infoConfig = async (settings) => {
+
+    const statechainEntityUrl = settings.statechainEntity;
+    const path = "info/config";
+
+    // Not working very well for signer
+    // TODO: Uncommenting this when changing to mainnet or testnet
+    /* let response = await axios.get(`${settings.esploraServer}/api/fee-estimates`);
+
+    const feeRateSatsPerByte = response.data[3];
+
+    console.log(`feeRateSatsPerByte: ${feeRateSatsPerByte}`) */
+
+    let response = await axios.get(`${settings.esploraServer}/api/v1/fees/recommended`);
+    
+    const feeRateSatsPerByte = parseInt(response.data.fastestFee, 10);
+
+    response = await axios.get(statechainEntityUrl + '/' + path);
+
+    return {    
+        initlock: response.data.initlock,
+        interval: response.data.interval,
+        feeRateSatsPerByte,
+    };
+
+}
+
+const createActivity = (utxo, amount, action) => {
+
+    const activity = {
+        utxo,
+        amount,
+        action,
+        date: new Date().toISOString()
+    };
+
+    return activity;
+}
+
+const completeWithdraw = async (clientConfig, statechainId, signedStatechainId) => {
+
+    const url = `${clientConfig.statechainEntity}/withdraw/complete`;
+
+    let deleteStatechainPayload = {
+        statechain_id: statechainId,
+        signed_statechain_id: signedStatechainId
+    };
+
+    await axios.post(url, deleteStatechainPayload);
+}
+
+const getStatechainInfo = async (clientConfig, statechainId) => {
+
+    const statechainEntityUrl = clientConfig.statechainEntity;
+    const path = `info/statechain/${statechainId}`;
+
+    try {
+        let response = await axios.get(statechainEntityUrl + '/' + path);
+        return response.data;
+    } catch (error) {
+        if (error.response.status == 404) {
+            return null;
+        } else {
+            throw error;
+        }
+    }
+}
+
+export default { infoConfig, createActivity, completeWithdraw, getStatechainInfo }
