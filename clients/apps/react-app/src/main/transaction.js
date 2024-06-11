eeeae6cdee83f441b7301ae21b9a6fbd98be7a3a
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2024-06-11 12:10:23 +0530

    fix: enclose post request of sign into try catch

--- clients/apps/react-app/src/main/transaction.js
+++ clients/apps/react-app/src/main/transaction.js
@@ -19,8 +19,16 @@ const signFirst = async (signFirstRequestPayload) => {
         socksAgent = { httpAgent: new SocksProxyAgent(torProxy) };
     }
     
-    let response = await axios.post(url, signFirstRequestPayload, socksAgent);
-
+    try {
+        let response = await axios.post(url, signFirstRequestPayload, socksAgent);
+        console.log('Response:', response.data);
+    } catch (error) {
+        if (error.code === 'ECONNREFUSED') {
+            console.error('Error: Connection refused. The server at 0.0.0.0:8000 is not available.');
+        } else {
+            console.error('An error occurred:', error.message);
+        }
+    }
     let server_pubnonce_hex = response.data.server_pubnonce;
 
     return server_pubnonce_hex;
@@ -40,8 +48,16 @@ const signSecond = async (partialSigRequest) => {
         socksAgent = { httpAgent: new SocksProxyAgent(torProxy) };
     }
 
-    let response = await axios.post(url, partialSigRequest, socksAgent);
-
+    try {
+        let response = await axios.post(url, partialSigRequest, socksAgent);
+        console.log('Response:', response.data);
+    } catch (error) {
+        if (error.code === 'ECONNREFUSED') {
+            console.error('Error: Connection refused. The server at 0.0.0.0:8000 is not available.');
+        } else {
+            console.error('An error occurred:', error.message);
+        }
+    }
     let server_partial_sig_hex = response.data.partial_sig;
 
     return server_partial_sig_hex;
