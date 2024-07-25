a6083339a6aa1380ea856297adb89fd6eb4326fb
S. Santos <ssantosj21@gmail.com>
2024-07-25 08:15:09 -0300

    Add last test

--- clients/apps/nodejs/test/ta02-duplicate-deposits.mjs
+++ clients/apps/nodejs/test/ta02-duplicate-deposits.mjs
@@ -61,11 +61,11 @@ describe('TA02 - Duplicated Deposits', function() {
                     "You will no longer be able to move other duplicate coins with the same statechain_id and this will cause PERMANENT LOSS of these duplicate coin funds.");
             }
 
-            let withdrawAddress = "bcrt1qn5948np2j8t68xgpceneg3ua4wcwhwrsqj8scv";
+            const withdrawAddress = "bcrt1qn5948np2j8t68xgpceneg3ua4wcwhwrsqj8scv";
 
             let txid = await mercurynodejslib.withdrawCoin(clientConfig, wallet1.name, duplicatedCoin.statechain_id, withdrawAddress, null, 1);
 
-            expect(txid).to.not.be.string;
+            expect(txid).to.be.string;
 
             try {
                 await mercurynodejslib.transferSend(clientConfig, wallet1.name, newCoin.statechain_id, transferAddress.transfer_receive, false, null);
@@ -76,7 +76,91 @@ describe('TA02 - Duplicated Deposits', function() {
             
             txid = await mercurynodejslib.withdrawCoin(clientConfig, wallet1.name, duplicatedCoin.statechain_id, withdrawAddress, null, 0);
 
-            expect(txid).to.not.be.string;
+            expect(txid).to.be.string;
+        })
+    }),
+
+    context('Transfer Flow', () => {
+        it('should complete successfully', async () => {
+
+            const clientConfig = client_config.load();
+            await removeDatabase(clientConfig);
+
+            let wallet_1_name = "w1";
+            let wallet_2_name = "w2";
+            let wallet1 = await createWallet(clientConfig, wallet_1_name);
+            let wallet2 = await createWallet(clientConfig, wallet_2_name);
+
+            const token = await mercurynodejslib.newToken(clientConfig, wallet1.name);
+            const tokenId = token.token_id;
+
+            const amount1 = 10000;
+            const depositInfo = await mercurynodejslib.getDepositBitcoinAddress(clientConfig, wallet1.name, amount1);
+
+            await depositCoin(depositInfo.deposit_address, amount1);
+
+            let amount2 = 20000;
+            await depositCoin(depositInfo.deposit_address, amount2);
+
+            const coreWalletAddress = await getnewaddress();
+
+            await generateBlock(clientConfig.confirmationTarget, coreWalletAddress);
+
+            let listCoins = await mercurynodejslib.listStatecoins(clientConfig, wallet1.name);
+
+            expect(listCoins.length).to.equal(2);
+
+            const newCoin = listCoins.find(coin => 
+                coin.aggregated_address == depositInfo.deposit_address && 
+                coin.status == CoinStatus.CONFIRMED
+            );
+              
+            let duplicatedCoin = listCoins.find(coin => 
+                coin.aggregated_address == depositInfo.deposit_address && 
+                coin.status == CoinStatus.DUPLICATED
+            );
+
+            expect(newCoin).to.not.be.null;
+            expect(duplicatedCoin).to.not.be.null;
+
+            expect(newCoin.duplicate_index).to.equal(0);
+            expect(duplicatedCoin.duplicate_index).to.equal(1);
+
+            const transferAddress = await mercurynodejslib.newTransferAddress(clientConfig, wallet2.name, null);
+
+            let result = await mercurynodejslib.transferSend(clientConfig, wallet1.name, newCoin.statechain_id, transferAddress.transfer_receive, true, null);
+            expect(result).to.have.property('statechain_id');
+
+            let transferReceiveResult = await mercurynodejslib.transferReceive(clientConfig, wallet2.name);
+
+            expect(transferReceiveResult.receivedStatechainIds).contains(newCoin.statechain_id);
+            expect(transferReceiveResult.receivedStatechainIds.length).to.equal(1);
+
+            listCoins = await mercurynodejslib.listStatecoins(clientConfig, wallet1.name);
+
+            const transferredCoin = listCoins.find(coin => 
+                coin.aggregated_address === depositInfo.deposit_address && 
+                coin.status === CoinStatus.TRANSFERRED
+            );
+              
+            duplicatedCoin = listCoins.find(coin => 
+                coin.aggregated_address === depositInfo.deposit_address && 
+                coin.status === CoinStatus.DUPLICATED
+            );
+
+            expect(transferredCoin).to.not.be.null;
+            expect(transferredCoin.duplicate_index).to.equal(0);
+
+            expect(duplicatedCoin).to.not.be.null;
+            expect(duplicatedCoin.duplicate_index).to.equal(1);
+
+            try {
+                const withdrawAddress = "bcrt1qn5948np2j8t68xgpceneg3ua4wcwhwrsqj8scv";
+                await mercurynodejslib.withdrawCoin(clientConfig, wallet1.name, duplicatedCoin.statechain_id, withdrawAddress, null, 1);
+            } catch (error) {
+                expect(error.response.status).to.equal(401);
+                expect(error.response.data.message).to.equal("Signature does not match authentication key.");
+            }
         })
     })
 })
