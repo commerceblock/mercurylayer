6e2241aeedbf05741a9d3d87c7906c23fc0c3335
S. Santos <ssantosj21@gmail.com>
2024-08-13 19:26:03 -0300

    fix: The pre-image is now only revealed if the transfer is unlocked.

--- server/src/endpoints/sign.rs
+++ server/src/endpoints/sign.rs
@@ -78,8 +78,6 @@ pub async fn sign_first(statechain_entity: &State<StateChainEntity>, sign_first_
         },
     };
 
-    println!("value: {}", value);
-
     let response: mercurylib::transaction::SignFirstResponsePayload = serde_json::from_str(value.as_str()).expect(&format!("failed to parse: {}", value.as_str()));
 
     let mut server_pubnonce_hex = response.server_pubnonce.clone();
@@ -153,8 +151,6 @@ pub async fn sign_second (statechain_entity: &State<StateChainEntity>, partial_s
             text
         },
         Err(err) => {
-            println!("ERROR sig: {}", err);
-
             let response_body = json!({
                 "error": "Internal Server Error",
                 "message": err.to_string()
